635f431ebc6a500ac26f1413a3e79341bd5cee89
zolova <ariel.malka@outlook.com>
2025-09-19 16:54:49 -0600

    Create home.js

---
+++ home.js
@@ -0,0 +1,25 @@
+document.querySelector('.footer-youtube').addEventListener('click', function() {
+  window.open('https://www.youtube.com/@Zolova', '_blank');
+});
+
+document.querySelector('.footer-email').addEventListener('click', function() {
+  window.open('mailto:zolova.development@outlook.com', '_blank');
+});
+
+document.querySelector('.footer-discord').addEventListener('click', function() {
+  window.location.href = "error.html";
+});
+
+
+document.querySelector('.youtube-button').addEventListener('click', function() {
+  window.open('https://www.youtube.com/@Zolova', '_blank');
+});
+document.querySelector('.spotify-button').addEventListener('click', function() {
+  window.open('https://open.spotify.com/artist/1YoFo8oLyWqGauioOlyHuX?si=222261fe24d14a85', '_blank');
+});
+document.querySelector('.twitch-button').addEventListener('click', function() {
+  window.open('https://www.twitch.tv/zack6038');
+});
+document.querySelector('.itchio-button').addEventListener('click', function() {
+  window.open('https://zack6038exe.itch.io', '_blank');
+});
